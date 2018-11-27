fcfae84a31a2ad7c7530d68ded8709cdb1f48517
puneetneu <tanwar.p@husky.neu.edu>
2018-11-27 15:28:44 -0500

    adding backend and server functionality for customers

--- final-project-backend/app.js
+++ final-project-backend/app.js
@@ -3,15 +3,24 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
 var path = require('path');
+var upload = require('express-fileupload');
+var multer = require("multer");
+
+var doctorController= require('./controllers/doctorController');
+var imagecontroller = require('./controllers/imagecontroller');
+
 
 var app = express();
-
+app.use(upload());
 const route=require('./routes/route');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api',route);
+
+app.use('/doctors', doctorController);
+app.use('/img', imagecontroller);
 
 //connect to mongoose
 mongoose.connect('mongodb://localhost:27017/finalproject', { useNewUrlParser: true });
