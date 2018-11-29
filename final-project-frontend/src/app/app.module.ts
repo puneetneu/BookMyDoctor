a9d6c71ab266f04d00247beda2e6920a12e9bcab
MamtaHate <mamtahate@gmail.com>
2018-11-29 18:35:02 -0500

    Adding profile page for Customer

--- final-project-frontend/src/app/app.module.ts
+++ final-project-frontend/src/app/app.module.ts
@@ -1,36 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { LoginCreateComponent } from './Homepage/login-create/login-create.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+import { FooterComponent } from './Homepage/footer/footer.component';
+import { DoctorRegistrationComponent } from './Homepage/doctor-registration/doctor-registration.component';
+import { HomeComponent } from './Homepage/home/home.component';
+import { CustomerProfileComponent } from './Customer/customer-profile/customer-profile.component';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { CustomerHomepageComponent } from './Customer/customer-homepage/customer-homepage.component';
+import { CustomerAppointmentsComponent } from './Customer/customer-appointments/customer-appointments.component';
+import { AuthInterceptor } from './Homepage/auth-interceptor';
 import { ClinicComponent } from './Doctor/clinic/clinic.component';
 import { DemographicComponent } from './Doctor/demographic/demographic.component';
+import { MainComponent } from './Doctor/main/main.component';
 import { EducationComponent } from './Doctor/education/education.component';
-import { SidebarComponent } from './Doctor/sidebar/sidebar.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './material-module';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginCreateComponent,
+    FooterComponent,
+    DoctorRegistrationComponent,
+    HomeComponent,
+    CustomerProfileComponent,
+    CustomerHomepageComponent,
+    CustomerAppointmentsComponent,
     ClinicComponent,
     DemographicComponent,
-    EducationComponent,
-    SidebarComponent
+    MainComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
-    DemoMaterialModule,
-    HttpClientModule
-    
+    MaterialModule,
+    LayoutModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
