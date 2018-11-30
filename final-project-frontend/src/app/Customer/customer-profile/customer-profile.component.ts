8f1e6fe6104bda8f142ffaa039487f9e29210635
MamtaHate <mamtahate@gmail.com>
2018-11-29 23:38:36 -0500

    test

--- final-project-frontend/src/app/Customer/customer-profile/customer-profile.component.ts
+++ final-project-frontend/src/app/Customer/customer-profile/customer-profile.component.ts
@@ -52,7 +52,13 @@ export class CustomerProfileComponent implements OnInit {
     .subscribe(arg => this.customerData$ = arg);
   }
 
+  onFormSubmit(firstForm: NgForm) {
 
+  }
+
+  secondFormSubmit(secondForm: NgForm) {
+
+  }
 
   finalFormSubmit(stepper: NgForm) {
     if (stepper.invalid) {
