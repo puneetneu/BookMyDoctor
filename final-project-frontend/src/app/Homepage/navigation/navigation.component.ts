1a90010023ca87adf60443b1a22050cba91f9151
MamtaHate <mamtahate@gmail.com>
2018-12-11 15:56:56 -0500

    adding directions on map

--- final-project-frontend/src/app/Homepage/navigation/navigation.component.ts
+++ final-project-frontend/src/app/Homepage/navigation/navigation.component.ts
@@ -8,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
 
   public origin: any
-public destination: any
+  public destination: any
   constructor() { }
 
   ngOnInit() {
   }
+
+  public coordinates: [
+    {
+     latitude: 24.799448,
+     longitude: 120.979021,
+    },
+    {
+      latitude: 24.799448,
+      longitude: 120.979021,
+     }
+   ];
 
   getDirection() {
     this.origin = { lat: 24.799448, lng: 120.979021 }
