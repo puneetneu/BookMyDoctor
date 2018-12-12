8e8a4198f83513fdc3c2b9a2ca15ab7ad15325c0
rajasiakolkar <43155316+rajasiakolkar@users.noreply.github.com>
2018-12-12 09:46:54 -0500

    Changes in service class to show handle user authentication

--- final-project-frontend/src/app/Homepage/auth.service.ts
+++ final-project-frontend/src/app/Homepage/auth.service.ts
@@ -101,7 +101,7 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: Auth = {email_id: email, password: password};
-    this.http.post<{token: string, type: string, userID: string, msg: string}>
+    return this.http.post<{token: string, type: string, userID: string, msg: string}>
     ('http://localhost:3000/login', authData).subscribe(response => {
           this.token = response.token;
           this.msg = response.msg;
